976e9021f816c2361f520528d67c3f52279d7483
Tishakaur <151868915+Tishakaur@users.noreply.github.com>
2023-11-25 22:34:38 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,11 @@
+Empty_tuple =  ()
+print(Empty_tuple)
+
+my_tuple1 =  ( 20, 17, 12)
+print(my_tuple1)
+
+my_tuple2 = ("Hello","World", 2023)
+print(my_tuple2)
+
+my_tuple3 = ("Newyear",(6,"Peace",90),[2,5,0])
+print(my_tuple3)
