7ab161c34a4982f700c26a74230d91ede6205da0
Tishakaur <151868915+Tishakaur@users.noreply.github.com>
2023-11-25 20:21:40 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+a1 = {"name": "John", "age":25,"marks":60}
+a2 = {"name": "Marsh","age":23 , "marks":75}
+a3 = {"name": "Max", "age":20, "marks":70}
+
+b1 = {"name": "steve","age":13,"marks":59}
+b2 = {"name":"Mitchell","age":22,"marks":74}
+b3 = {"name":"Head","age":19,"marks":65}
+
+c1 = {"name": "shine","age":27,"marks":100}
+C2 = {"name": "Root","age":26,"marks":89}
+c3 = {"name": "sia ","age":18,"marks":40}
+
+studentsA = { 1:a1 ,2:a2, 3:a3}
+studentsB = { 1:b1 ,2:b2 ,3:b3}
+studentsC = { 1:c1 ,2:C2, 3:c3}
+
+print("Records of Class A")
+print(studentsA[1])
+print(studentsA[2])
+print(studentsA[3])
+
+print("Records of Class B")
+print(studentsB[1])
+print(studentsB[2])
+print(studentsB[3])
+
+print("Records of class CC")
+print(studentsC[1])
+print(studentsC[2])
+print(studentsC[3])
