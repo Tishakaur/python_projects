2eb49dcf088df091495b00e09032e754f54bc198
Tishakaur <151868915+Tishakaur@users.noreply.github.com>
2023-11-25 23:42:15 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,7 @@
+import random
+
+colors = ["red","blue","white","Black","Pink"]
+print("the list of colors is:",colors)
+
+color = random.choice(colors)
+print("The randomly selecteed value from the list is:",color)
