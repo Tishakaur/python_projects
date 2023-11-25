ff3de0494ce95f119d75eea77fda775b3b0db1fb
Tishakaur <151868915+Tishakaur@users.noreply.github.com>
2023-11-25 20:23:05 +0530

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,23 @@
+a = 12
+b = 5.0
+c = "7"
+
+a = float(a)
+b = float(b)
+c =  int(c)
+
+print(a)
+print(b)
+print(c*9)
+
+x = 23
+y = 4.0
+z = 932
+
+x = float(x)
+y = int(y)
+z = str(z)
+
+print(x)
+print(y/2)
+print(z*3)
