902803e8943c9c7156a6fc84334ad6465552f491
Tishakaur <151868915+Tishakaur@users.noreply.github.com>
2023-11-25 22:37:34 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,7 @@
+def Bonjour(First_name,last_name,age):
+    print("Bonjour"+" " +First_name," "+last_name)
+    print("you're"+" " +str(age)+"Years old")
+    print("Have a GOOD DAY!")
+
+Bonjour( "John","sir",40)
+
